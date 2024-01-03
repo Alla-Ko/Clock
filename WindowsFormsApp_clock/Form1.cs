984b2fe97507b9f1b0bf30f683fa08d75e3e88f0
Alla Kokhaniuk <121404468+Alla-Ko@users.noreply.github.com>
2024-01-03 18:28:42 +0200

    Add files via upload

---
+++ WindowsFormsApp_clock/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DateTime a = DateTime.Now;
+            label1.Text = a.ToLongTimeString();
+            vTimer.Tick += new EventHandler(ShowTime);
+            vTimer.Interval = 1000;
+            vTimer.Start();
+        }
+        private static Timer vTimer = new Timer();
+        private void ShowTime(object vObj, EventArgs e)
+        {
+            // перетворення в рядок
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+
+
+    }
+}
